c5bfd15cd479ebb0a6069fc7c109de73fc588794
Dana <clarosdana03@gmail.com>
2026-02-14 02:43:00 -0300

    repositories y DTOs de Caballo y Jinete hechos

---
+++ backend/WebApi/DTOs/Jinetes/JineteDto.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApi.DTOs.Jinetes
+{
+    public class JineteDto
+    {
+        public long Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        public decimal AlturaCm { get; set; }
+        public decimal PesoKg { get; set; }
+        public decimal LargoPierna { get; set; }
+        public decimal AnchoCadera { get; set; }
+        public string Nivel { get; set; } = null!;
+        public string Disciplina { get; set; } = null!;
+        public long UserId { get; set; }
+    }
+}
---
+++ backend/WebApi/Repositories/CaballoRepository.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
+using WebApi.Models;
+
+namespace WebApi.Repositories
+{
+    public class CaballoRepository : ICaballoRepository
+    {
+        private readonly NC07WebAppContext _context;
+
+        public CaballoRepository(NC07WebAppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Caballo?> GetByIdAsync(long id)
+        {
+            return await _context.Caballos.FindAsync(id);
+        }
+        public async Task<IEnumerable<Caballo>> GetAllAsync()
+        {
+            return await _context.Caballos.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Caballo>> FindAsync(System.Linq.Expressions.Expression<Func<Caballo, bool>> predicate)
+        {
+            return await _context.Caballos.Where(predicate).ToListAsync();
+        }
+
+        public async Task<Caballo?> FirstOrDefaultAsync(System.Linq.Expressions.Expression<Func<Caballo, bool>> predicate)
+        {
+            return await _context.Caballos.FirstOrDefaultAsync(predicate);
+        }
+
+        public async Task<Caballo> AddAsync(Caballo entity)
+        {
+         //   entity.CreatedAt = DateTime.Now;
+         //   entity.ModifiedAt = DateTime.Now;
+            await _context.Caballos.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task UpdateAsync(Caballo entity)
+        {
+          //  entity.ModifiedAt = DateTime.Now;
+            _context.Caballos.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Caballo entity)
+        {
+            _context.Caballos.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> ExistsAsync(System.Linq.Expressions.Expression<Func<Caballo, bool>> predicate)
+        {
+            return await _context.Caballos.AnyAsync(predicate);
+        }
+
+    }
+}
